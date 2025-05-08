d405e59d329423fa822fffefeccfd9e7ac5b693f
Jose Alvarez <josealvarezvivar@gmail.com>
2025-05-08 18:06:16 +0200

    Added uptade.

---
+++ SuperDuper/Base/DBModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperDuper.Base
+{
+    public class DBModel
+    {
+        public string[] Filters { get; set; }
+    }
+}
--- SuperDuper/DAL/QueryFormatter.cs
+++ SuperDuper/DAL/QueryFormatter.cs
@@ -1,4 +1,5 @@
 ﻿using SuperDuper.Resources;
+using System.Reflection;
 
 namespace SuperDuper.DAL
 {
@@ -35,20 +36,9 @@ namespace SuperDuper.DAL
             var properties = type.GetProperties();
 
             string query = "INSERT INTO {0} ({1}) VALUES ({2});";
-            string fields = string.Empty;
-            string values = string.Empty;
 
-            foreach (var property in properties)
-            {
-                fields += $"{property.Name.ToSnakeCase()}, ";
-            }
-            fields = fields.Substring(0, fields.LastIndexOf(','));
-
-            foreach (var property in properties)
-            {
-                values += $"@{property.Name}, ";
-            }
-            values = values.Substring(0, values.LastIndexOf(","));
+            string fields = GetFields(properties);
+            string values = GetValues(properties);
 
             query = string.Format(query, table, fields, values);
 
@@ -62,12 +52,87 @@ namespace SuperDuper.DAL
 
         private static Result<string> GetUpdate<T>(T model, string table, bool applyNulls)
         {
-            return Result<string>.Failure("");
+            var type = typeof(T);
+            var properties = type.GetProperties();
+            string query = "UPDATE {0} SET {1}";
+
+            string fields = GetFields(properties);
+            string values = GetFieldWithValues(properties);
+            string filters = GetFilters(model, properties);
+
+            query = string.Format(query, values);
+
+            if(!string.IsNullOrEmpty(filters))
+                query += $" WHERE {filters}";
+
+            query += ";";
+
+            return Result<string>.Success(query);
         }
 
         private static Result<string> GetDelete<T>(T model, string table, bool applyNulls)
         {
             return Result<string>.Failure("");
+        }
+
+        private static string GetFields(PropertyInfo[] properties)
+        {
+            string fields = string.Empty;
+
+            foreach (var property in properties)
+            {
+                fields += $"{property.Name.ToSnakeCase()}, ";
+            }
+            fields = fields.Substring(0, fields.LastIndexOf(','));
+
+            return fields;
+        }
+
+        private static string GetValues(PropertyInfo[] properties)
+        {
+            string values = string.Empty;
+
+            foreach (var property in properties)
+            {
+                values += $"@{property.Name}, ";
+            }
+            values = values.Substring(0, values.LastIndexOf(","));
+
+            return values;
+        }
+
+        private static string GetFieldWithValues(PropertyInfo[] properties)
+        {
+            string values = string.Empty;
+
+            foreach (var property in properties)
+            {
+                values += $"{property.Name} = @{property.Name}, ";
+            }
+            values = values.Substring(0, values.LastIndexOf(","));
+
+            return values;
+        }
+
+        private static string GetFilters<T>(T model, PropertyInfo[] properties)
+        {
+            string filters = string.Empty;
+
+            var filterProperty = properties.Where(p => p.Name == "Filters").FirstOrDefault();
+            if(filterProperty == null)
+                return filters;
+
+            var values = filterProperty.GetValue(model);
+            if (values == null || values.GetType() != typeof(List<string>))
+                return filters;
+
+            List<string> list = values != null ? (List<string>)values : new List<string>();
+            foreach(var currentFilter in list)
+                filters += $"{currentFilter} = @{currentFilter} AND ";
+            filters = filters.Substring(0, filters.Length - 4);
+
+            return filters;
+
         }
     }
 }
